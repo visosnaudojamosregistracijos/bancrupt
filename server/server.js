e9414321ba392ebcf3a137ca5441c9db3491b660
Tomas <visosnaudojamosregistracijos@gmail.com>
2026-09-10 19:46:57 +0300

    Client perkeltas i server/public

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // STATINIŲ FAILŲ APTARNAVIMAS
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Atsarginis route'as
 app.get('/', (req, res) => {
